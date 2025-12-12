74f3ff1b814326da4f99f4f0ce15e14cb8069ff9
Gustavo Toffano <168567532+gustavomasson4@users.noreply.github.com>
2025-12-12 14:05:53 -0300

    mudança do repositorio do backend

--- backend/CalculadoraApi/Models/CalculoRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace CalculadoraApi.Models
-{
-    public class CalculoRequest
-    {
-        public double PrimeiroNumero { get; set; }
-        public double SegundoNumero { get; set; }
-        public string Operacao { get; set; } = string.Empty;
-    }
-}
--- backend/CalculadoraApi/Models/Operacao.cs
+++
@@ -1,8 +0,0 @@
-namespace CalculadoraApi.Models
-{
-    public class Operacao
-    {
-        public string Nome { get; set; } = string.Empty;
-        public string Valor { get; set; } = string.Empty;
-    }
-}
